a62bb6da8e7574623350b04c07eb818c9d73e416
Nicolas Sarcevic <nicolas@humanapis.io>
2023-07-19 12:26:21 -0400

    cambios internos

--- ubuntu.sh
+++ ubuntu.sh
@@ -129,8 +129,8 @@ function main {
   done
 
   f_pre
-  f_kernel
-  f_firewall
+#  f_kernel
+#  f_firewall
   f_disablenet
   f_disablefs
   f_disablemod
@@ -139,48 +139,48 @@ function main {
   f_logindconf
   f_journalctl
   f_timesyncd
-  f_fstab
-  f_prelink
+#  f_fstab
+#  f_prelink
   f_aptget_configure
   f_aptget
-  f_hosts
+#  f_hosts
   f_issue
   f_sudo
   f_logindefs
   f_sysctl
-  f_limitsconf
-  f_adduser
-  f_rootaccess
+#  f_limitsconf
+#  f_adduser
+#  f_rootaccess
   f_package_install
-  f_psad
-  f_coredump
-  f_usbguard
-  f_postfix
+#  f_psad
+#  f_coredump
+#  f_usbguard
+#  f_postfix
   f_apport
   f_motdnews
   f_rkhunter
   f_sshconfig
   f_sshdconfig
   f_password
-  f_cron
+#  f_cron
   f_ctrlaltdel
-  f_auditd
-  f_aide
+#  f_auditd
+#  f_aide
   f_rhosts
   f_users
-  f_lockroot
+#  f_lockroot
   f_package_remove
   f_suid
   f_restrictcompilers
   f_umask
-  f_path
-  f_aa_enforce
-  f_aide_post
-  f_aide_timer
-  f_aptget_noexec
+#  f_path
+#  f_aa_enforce
+#  f_aide_post
+#  f_aide_timer
+#  f_aptget_noexec
   f_aptget_clean
-  f_systemddelta
-  f_post
+#  f_systemddelta
+#  f_post
   f_checkreboot
 
   echo
